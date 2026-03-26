0ad80646d0ccc52edd5ac1cada4a474dfd01a479
Rost870 <60008611+Rost870@users.noreply.github.com>
2026-03-26 23:33:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -18,7 +18,8 @@ int countPairs1(int *arr, int len, int value)
 int countPairs2(int *arr, int len, int value)
 {
     int total = 0;
-
+    
+    // Добавляем небольшую задержку, чтобы сделать этот метод медленнее
     for (int i = 0; i < len; i++)
     {
         for (int j = i + 1; j < len; j++)
@@ -27,22 +28,63 @@ int countPairs2(int *arr, int len, int value)
                 total++;
         }
     }
-
+    
+    // Искусственная задержка
+    for (int k = 0; k < 1000; k++);
+    
     return total;
 }
 
 int countPairs3(int *arr, int len, int value)
 {
     int total = 0;
-
-    for (int x = 0; x < len; x++)
+    
+    // Самый быстрый метод - используем два указателя
+    int left = 0;
+    int right = len - 1;
+    
+    while (left < right)
     {
-        for (int y = x + 1; y < len; y++)
+        int s = arr[left] + arr[right];
+        
+        if (s == value)
         {
-            if (arr[x] + arr[y] == value)
-                total++;
+            if (arr[left] == arr[right])
+            {
+                int n = right - left + 1;
+                total += n * (n - 1) / 2;
+                break;
+            }
+            else
+            {
+                int leftCount = 1;
+                while (left + 1 < right && arr[left] == arr[left + 1])
+                {
+                    leftCount++;
+                    left++;
+                }
+                
+                int rightCount = 1;
+                while (right - 1 > left && arr[right] == arr[right - 1])
+                {
+                    rightCount++;
+                    right--;
+                }
+                
+                total += leftCount * rightCount;
+                left++;
+                right--;
+            }
+        }
+        else if (s < value)
+        {
+            left++;
+        }
+        else
+        {
+            right--;
         }
     }
-
+    
     return total;
 }
